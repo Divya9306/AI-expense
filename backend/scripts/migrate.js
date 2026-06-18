a6ddda38d8e31074f3b2ccb5bbcb946d38915759
unknown <divyakaur2005@gmail.com>
2026-06-18 21:03:40 +0530

    added the sql file

--- backend/scripts/migrate.js
+++ backend/scripts/migrate.js
@@ -2,3 +2,26 @@ import fs from 'fs/promises';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import pool from '../utils/db.js';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = path.dirname(_filename);
+
+const DROP_ALL= `
+    DROP TABLE IF EXISTS ai_insights CASCADE;
+    DROP TABLE IF EXISTS BUDGETS CASCADE;
+    DROP TABLE IF EXISTS TRANSACTIONS CASCADE;
+    DROP TABLE IF EXISTS CATEGORIES CASCADE;
+    DROP TABLE IF EXISTS USERS CASCADE;
+`;
+
+const runMigration =async() =>{
+    const shouldReset = process.argv.includes("--reste");
+    const schemaPath = path.join(_dirname, "..", "sql", "schema.sql");
+
+try {
+    if (shouldReset) {
+        console.log("Dropping existing tables...");
+        await pool.query(DROP_ALL);
+    }
+}
+}
